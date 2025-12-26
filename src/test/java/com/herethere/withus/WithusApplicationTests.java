e037c1803832df904a1692de69d71061483a47c2
2jiyong <jdragon2000@naver.com>
2025-12-26 17:14:01 +0900

    chore: init

---
+++ src/test/java/com/herethere/withus/WithusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herethere.withus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
