f31ec130d6c0e145c9ab6d9e2e5c4d335b18ac64
2jiyong <jdragon2000@naver.com>
2026-01-06 00:50:40 +0900

    feat: AuthService 임시 구현

--- src/main/java/com/herethere/withus/auth/service/AuthService.java
+++ src/main/java/com/herethere/withus/auth/service/AuthService.java
@@ -2,9 +2,24 @@ package com.herethere.withus.auth.service;
 
 import org.springframework.stereotype.Service;
 
+import com.herethere.withus.auth.domain.OAuthProviderType;
+import com.herethere.withus.auth.dto.internal.OAuthUserInfo;
+import com.herethere.withus.auth.dto.request.LoginRequest;
+import com.herethere.withus.auth.dto.response.LoginResponse;
+import com.herethere.withus.auth.oauthclient.OAuthClient;
+import com.herethere.withus.auth.oauthclient.OAuthClientFactory;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+	private final OAuthClientFactory oauthClientFactory;
+
+	public LoginResponse login(LoginRequest request, OAuthProviderType provider) {
+		OAuthClient oauthClient = oauthClientFactory.getOAuthClient(provider);
+		OAuthUserInfo userInfo = oauthClient.getUserInfo(request.oauthToken());
+		// TODO:추후작업
+		return null;
+	}
 }
