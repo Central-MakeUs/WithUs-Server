a581911de5e78cdc32a3c696acee2e4046600a26
2jiyong <jdragon2000@naver.com>
2026-01-06 21:58:58 +0900

    feat: valid 조건에 message 추가

--- src/main/java/com/herethere/withus/auth/dto/request/LoginRequest.java
+++ src/main/java/com/herethere/withus/auth/dto/request/LoginRequest.java
@@ -3,6 +3,6 @@ package com.herethere.withus.auth.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-	@NotBlank String oauthToken,
+	@NotBlank(message = "OAuth Token이 존재하지 않습니다.") String oauthToken,
 	String fcmToken) { //TODO : valid 관련 어노테이션 추가하기
 }
