fe78393008cc3423faefd89c951e51b959c805a4
2jiyong <jdragon2000@naver.com>
2026-01-06 22:14:10 +0900

    feat: swagger Bearer auth 설정 추가

---
+++ src/main/java/com/herethere/withus/common/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.herethere.withus.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+	private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+			.components(new Components()
+				.addSecuritySchemes(
+					SECURITY_SCHEME_NAME,
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")
+				)
+			);
+	}
+}
