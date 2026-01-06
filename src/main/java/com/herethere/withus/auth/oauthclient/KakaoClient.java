c942b735654764043a097013eea2f83f4b5cfbe5
2jiyong <jdragon2000@naver.com>
2026-01-06 16:42:13 +0900

    feat: KakaoClient 구현

--- src/main/java/com/herethere/withus/auth/oauthclient/KakaoClient.java
+++ src/main/java/com/herethere/withus/auth/oauthclient/KakaoClient.java
@@ -2,12 +2,20 @@ package com.herethere.withus.auth.oauthclient;
 
 import org.springframework.stereotype.Service;
 
+import com.herethere.withus.auth.dto.internal.KakaoUserInfo;
 import com.herethere.withus.auth.dto.internal.OAuthUserInfo;
+import com.herethere.withus.auth.externalapi.KakaoApiClient;
+
+import lombok.RequiredArgsConstructor;
 
 @Service("kakao")
+@RequiredArgsConstructor
 public class KakaoClient implements OAuthClient {
+	private final KakaoApiClient kakaoApiClient;
+
 	@Override
 	public OAuthUserInfo getUserInfo(String oauthToken) {
-		return null;
+		KakaoUserInfo userInfo = kakaoApiClient.getUser(oauthToken);
+		return new OAuthUserInfo(userInfo.id());
 	}
 }
