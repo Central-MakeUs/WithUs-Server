7411d70d90d36d6152190b83385ad498f4c584cf
2jiyong <jdragon2000@naver.com>
2026-01-07 21:58:31 +0900

    feat: EnableJpaAuditing 설정 추가

--- src/main/java/com/herethere/withus/WithusApplication.java
+++ src/main/java/com/herethere/withus/WithusApplication.java
@@ -3,9 +3,11 @@ package com.herethere.withus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class WithusApplication {
 
 	public static void main(String[] args) {
