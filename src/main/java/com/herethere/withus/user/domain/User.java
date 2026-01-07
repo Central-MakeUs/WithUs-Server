50bac0f41532879e96b53765932c8cbb41a29e66
2jiyong <jdragon2000@naver.com>
2026-01-07 23:11:15 +0900

    fix: updateProfile 메서드에서 초기화 여부까지 바꾸도록 수정

--- src/main/java/com/herethere/withus/user/domain/User.java
+++ src/main/java/com/herethere/withus/user/domain/User.java
@@ -53,8 +53,9 @@ public class User extends BaseEntity {
 	@Column(name = "profile_image_url", length = 2048)
 	private String profileImageUrl;
 
-	public void updateProfile(String nickname, String profileImageUrl) {
+	public void initializeProfile(String nickname, String profileImageUrl) {
 		this.nickname = nickname;
 		this.profileImageUrl = profileImageUrl;
+		isInitialized = true;
 	}
 }
