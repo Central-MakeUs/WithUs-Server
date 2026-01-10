970cfe52c5b3266b581964af615bcf1eae742594
2jiyong <jdragon2000@naver.com>
2026-01-10 11:17:39 +0900

    feat: 닉네임 valid 추가

--- src/main/java/com/herethere/withus/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/herethere/withus/user/dto/request/UserUpdateRequest.java
@@ -1,7 +1,10 @@
 package com.herethere.withus.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record UserUpdateRequest(@NotBlank(message = "닉네임은 필수 항목입니다.") String nickname,
+public record UserUpdateRequest(@NotBlank(message = "닉네임은 필수 항목입니다.")
+								@Size(min = 2, max = 8, message = "닉네임은 2자 이상 8자 이하로 입력해주세요.")
+								String nickname,
 								String imageObjectKey) {
 }
