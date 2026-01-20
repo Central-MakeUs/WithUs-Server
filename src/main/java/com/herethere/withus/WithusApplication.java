05f48457eecd019940c80766d21eb1301ed68de0
2jiyong <jdragon2000@naver.com>
2026-01-21 01:26:08 +0900

    feat: 기본 시간대 utc로 설정

--- src/main/java/com/herethere/withus/WithusApplication.java
+++ src/main/java/com/herethere/withus/WithusApplication.java
@@ -1,9 +1,13 @@
 package com.herethere.withus;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,6 +16,12 @@ public class WithusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WithusApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		// 애플리케이션의 기본 시간대를 UTC로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
