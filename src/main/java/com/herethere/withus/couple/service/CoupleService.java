3696d6d947b7441373b9ab3c8001543e12cf5119
2jiyong <jdragon2000@naver.com>
2026-01-21 22:37:39 +0900

    feat: 응답에 정렬 추가

--- src/main/java/com/herethere/withus/couple/service/CoupleService.java
+++ src/main/java/com/herethere/withus/couple/service/CoupleService.java
@@ -2,25 +2,18 @@ package com.herethere.withus.couple.service;
 
 import static com.herethere.withus.common.exception.ErrorCode.*;
 
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.herethere.withus.common.exception.ConflictException;
 import com.herethere.withus.common.exception.NotFoundException;
 import com.herethere.withus.couple.domain.Couple;
-import com.herethere.withus.couple.domain.CoupleKeyword;
-import com.herethere.withus.couple.domain.CoupleStatus;
-import com.herethere.withus.couple.dto.request.CoupleInitializeRequest;
 import com.herethere.withus.couple.dto.request.CoupleJoinPreviewRequest;
 import com.herethere.withus.couple.dto.request.CoupleJoinRequest;
 import com.herethere.withus.couple.dto.response.CoupleJoinPreviewResponse;
 import com.herethere.withus.couple.dto.response.CoupleJoinResponse;
 import com.herethere.withus.couple.repository.CoupleKeywordRepository;
 import com.herethere.withus.couple.repository.CoupleRepository;
-import com.herethere.withus.keyword.domain.Keyword;
 import com.herethere.withus.keyword.repository.KeywordRepository;
 import com.herethere.withus.keyword.service.KeywordService;
 import com.herethere.withus.user.domain.InviteCode;
@@ -81,36 +74,6 @@ public class CoupleService {
 		inviteCodeRepository.deleteByUser(receiver);
 
 		return new CoupleJoinResponse(couple.getId());
-	}
-
-	@Transactional
-	public void initializeCoupleSettings(CoupleInitializeRequest request) {
-		User user = userContextService.getCurrentUser();
-		Couple couple = user.getCouple();
-
-		if (couple == null) {
-			throw new NotFoundException(COUPLE_NOT_FOUND);
-		}
-		if (couple.getStatus() == CoupleStatus.ACTIVE) {
-			throw new ConflictException(COUPLE_ALREADY_INITIALIZED);
-		}
-		if (couple.getStatus() == CoupleStatus.DELETED) {
-			throw new ConflictException(COUPLE_DELETED);
-		}
-
-		Set<Keyword> chosenKeywordSet = keywordService.getChosenKeywords(request.defaultKeywordIds(),
-			request.customKeywords());
-
-		List<CoupleKeyword> coupleKeywords = chosenKeywordSet.stream()
-			.map(k -> CoupleKeyword.builder()
-				.keyword(k)
-				.couple(couple)
-				.build())
-			.toList();
-
-		coupleKeywordRepository.saveAll(coupleKeywords);
-
-		couple.initialize(request.questionTime());
 	}
 
 	private InviteCode getInviteCode(String code) {
