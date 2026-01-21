f23842ac3415df98c7200a5bb2ad997589ede1df
2jiyong <jdragon2000@naver.com>
2026-01-21 11:05:09 +0900

    fix: enableScheduling 추가하여 스케줄러 작동하도록 함

--- src/main/java/com/herethere/withus/WithusApplication.java
+++ src/main/java/com/herethere/withus/WithusApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableScheduling
 public class WithusApplication {
 
 	public static void main(String[] args) {
