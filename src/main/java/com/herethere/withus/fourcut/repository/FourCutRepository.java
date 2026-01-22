1103268c6b1bb71e3f0398dda230a049ce79df07
2jiyong <jdragon2000@naver.com>
2026-01-23 00:49:30 +0900

    feat: 네컷 엔티티 및 repo 생성

---
+++ src/main/java/com/herethere/withus/fourcut/repository/FourCutRepository.java
@@ -0,0 +1,8 @@
+package com.herethere.withus.fourcut.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.herethere.withus.fourcut.domain.FourCut;
+
+public interface FourCutRepository extends JpaRepository<FourCut, Long> {
+}
