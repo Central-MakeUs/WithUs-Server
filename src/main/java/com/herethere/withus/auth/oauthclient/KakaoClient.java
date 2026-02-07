a35eceb68cd23a9fcbaa92737360941b28efa95d
2jiyong <jdragon2000@naver.com>
2026-02-08 00:10:37 +0900

    feat: kakao adminKey 추가하고, 탈퇴 로직 구현

--- src/main/java/com/herethere/withus/auth/oauthclient/KakaoClient.java
+++ src/main/java/com/herethere/withus/auth/oauthclient/KakaoClient.java
@@ -1,18 +1,24 @@
 package com.herethere.withus.auth.oauthclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.herethere.withus.auth.dto.internal.KakaoUserInfo;
 import com.herethere.withus.auth.dto.internal.OAuthUserInfo;
+import com.herethere.withus.auth.dto.request.KakaoRevokeRequest;
 import com.herethere.withus.auth.externalapi.KakaoApiClient;
 import com.herethere.withus.user.domain.User;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service("kakao")
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoClient implements OAuthClient {
 	private final KakaoApiClient kakaoApiClient;
+	@Value("${oauth.kakao.app.adminKey}")
+	private String adminKey;
 
 	@Override
 	public OAuthUserInfo getUserInfo(String oauthToken, String authorizationCode) {
@@ -23,6 +29,13 @@ public class KakaoClient implements OAuthClient {
 
 	@Override
 	public void withdrawUser(User user) {
-
+		adminKey = "KakaoAK " + adminKey;
+		Long providerId = Long.valueOf(user.getProviderId());
+		KakaoRevokeRequest request = new KakaoRevokeRequest("user_id", providerId);
+		try {
+			kakaoApiClient.revoke(adminKey, request);
+		} catch (Exception e) {
+			log.error("Kakao 회원 탈퇴 실패 {}: {}", user.getId(), e.getMessage());
+		}
 	}
 }
