9c12f4bf2ea3b3f17f3c4f33faff3e11eb53dd53
2jiyong <jdragon2000@naver.com>
2026-02-14 17:06:53 +0900

    fix: refreshtoken 발급 api 필터 해제

--- src/main/java/com/herethere/withus/common/security/SecurityConfig.java
+++ src/main/java/com/herethere/withus/common/security/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
 			// 2. 인증/인가 설정
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/api/auth/logout").authenticated()
-				.requestMatchers("/api/auth/refresh").authenticated()
 				.requestMatchers("/api/auth/**").permitAll()
 				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
 				.requestMatchers("/invite/**").permitAll()
