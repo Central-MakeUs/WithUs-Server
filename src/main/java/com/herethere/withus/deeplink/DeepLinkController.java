6727b31b8e4d226d0b4e1350163a328b7f7ca54a
2jiyong <jdragon2000@naver.com>
2026-03-06 12:06:07 +0900

    feat: 앱에서 열기 추가

--- src/main/java/com/herethere/withus/deeplink/DeepLinkController.java
+++ src/main/java/com/herethere/withus/deeplink/DeepLinkController.java
@@ -1,13 +1,16 @@
 package com.herethere.withus.deeplink;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DeepLinkController {
 
 	@GetMapping("/invite")
-	public String invite() {
+	public String invite(@RequestParam(required = false) String code, Model model) {
+		model.addAttribute("code", code);
 		return "invite";
 	}
 }
